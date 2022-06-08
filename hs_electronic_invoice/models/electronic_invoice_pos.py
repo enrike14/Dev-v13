40a9762ca7593dad9110c7ad0ca2d2477997baf5
enrike14 <evelasquez1485@gmail.com>
2022-06-08 15:02:14 -0500

    fix pdf error

--- hs_electronic_invoice/models/electronic_invoice_pos.py
+++ hs_electronic_invoice/models/electronic_invoice_pos.py
@@ -14,5 +14,5 @@ class PosOrder(models.Model):
         for order in self:
             if order.account_move and order.account_move.type == 'out_invoice':
                 order.account_move.send_fiscal_doc()
-                order.account_move.get_pdf_fe()
+                # order.account_move.get_pdf_fe()
         return act_window
