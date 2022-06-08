296e4df92403a98c8ed0afb74a2c88f6e1c56995
enrike14 <evelasquez1485@gmail.com>
2022-06-08 10:37:05 -0500

    add pos code

--- hs_electronic_invoice/models/__init__.py
+++ hs_electronic_invoice/models/__init__.py
@@ -14,4 +14,4 @@ from . import electronic_invoice_province
 from . import electronic_invoice_district
 from . import electronic_invoice_sector
 from . import electronic_invoice_measures
-#from . import electronic_invoice_pos
+from . import electronic_invoice_pos
