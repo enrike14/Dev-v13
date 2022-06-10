fc6c328eb2cf9fcb7293ade2ebfa82a61a984d35
enrike14 <evelasquez1485@gmail.com>
2022-06-10 10:47:31 -0500

    test js

---
+++ hs_pos_fe/static/src/js/pos.js
@@ -0,0 +1,41 @@
+odoo.define("pos_fe.screens", function (require) {
+  "use strict";
+
+  var screens = require("point_of_sale.screens");
+
+  screens.ReceiptScreenWidget.include({
+    /*  printfe: function () {
+      console.log("Funciona JS");
+      //let self = this;
+      //let order = self.pos.get_order();
+
+      //const orderName = order.get_name();
+
+      await this.rpc({
+        model: 'pos.order',
+        method: 'action_print_fe',
+        args: [[orderName]],
+        kwargs: { context: this.env.session.user_context },
+        },{
+        timeout: 30000,
+        shadow: true,
+        }).then(function (file) {
+        if (file){
+          console.log(file);
+        //window.open(file.url);
+        }
+        }).catch(function (reason){
+        });
+    },*/
+
+    renderElement: function () {
+      let self = this;
+      this.$(".print-fe").click(function () {
+        console.log("el onclick");
+        if (!self._locked) {
+          //self.printfe();
+        }
+      });
+    },
+  });
+});
