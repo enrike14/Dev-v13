bcd3b9177b12b16f6130558bd676b64a39f2c439
enrike14 <evelasquez1485@gmail.com>
2022-06-10 15:09:38 -0500

    test js changes

--- hs_pos_fe/static/src/js/pos.js
+++ hs_pos_fe/static/src/js/pos.js
@@ -2,7 +2,7 @@ odoo.define("pos_fe.screens", function (require) {
   "use strict";
 
   var screens = require("point_of_sale.screens");
-
+  console.log("Funciona JS");
   screens.ReceiptScreenWidget.include({
     printfe: function () {
       console.log("Funciona JS");
