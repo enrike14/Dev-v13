13c28497f50bce6e4f6fcf235fd6aaf2bae52649
enrike14 <evelasquez1485@gmail.com>
2022-06-10 15:33:58 -0500

    test super

--- hs_pos_fe/static/src/js/pos.js
+++ hs_pos_fe/static/src/js/pos.js
@@ -3,5 +3,40 @@ odoo.define("pos_fe.screens", function (require) {
 
   var screens = require("point_of_sale.screens");
   console.log("Funciona JS");
-  screens.ReceiptScreenWidget.include({});
+  screens.ReceiptScreenWidget.include({
+    printfe: function () {
+      console.log("Funciona JS");
+      //let self = this;
+      //let order = self.pos.get_order();
+
+      //const orderName = order.get_name();
+
+      /*  await this.rpc({
+        model: 'pos.order',
+        method: 'action_print_fe',
+        args: [[orderName]],
+        kwargs: { context: this.env.session.user_context },
+        },{
+        timeout: 30000,
+        shadow: true,
+        }).then(function (file) {
+        if (file){
+          console.log(file);
+        //window.open(file.url);
+        }
+        }).catch(function (reason){
+        });*/
+    },
+
+    renderElement: function () {
+      var self = this;
+      this._super();
+      this.$(".button.printfe").click(function () {
+        console.log("el onclick");
+        if (!self._locked) {
+          //self.printfe();
+        }
+      });
+    },
+  });
 });
