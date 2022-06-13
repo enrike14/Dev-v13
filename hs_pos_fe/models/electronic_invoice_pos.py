98d788e689318ab881ab7408ca767d086ecdd14e
enrike14 <evelasquez1485@gmail.com>
2022-06-13 14:32:19 -0500

    test error change

--- hs_pos_fe/models/electronic_invoice_pos.py
+++ hs_pos_fe/models/electronic_invoice_pos.py
@@ -13,21 +13,6 @@ class PosOrder(models.Model):
         act_window = super(PosOrder, self).action_pos_order_invoice()
         for order in self:
             if order.account_move and order.account_move.type == 'out_invoice':
-                fe_info = order.account_move.get_fe_info()
-                for info in fe_info:
-
-                    if info == 'cafe':
-                        cafe = fe_info[info]
-
-                    if info == 'qr':
-                        qr = fe_info[info]
-
-                    if info == 'isPOS':
-                        if fe_info[info]:
-                            order.account_move.send_fiscal_doc()
-                            order.include_pos = fe_info[info]
-                            order.qr_code = qr
-                            order.CAFE = cafe
-                            logging.info("El CAFE del FE:" + order.CAFE)
+                order.account_move.send_fiscal_doc()
 
         return act_window
