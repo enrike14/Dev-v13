d10026df8828bcfe5f7b107cf5001aadbb593759
enrike14 <evelasquez1485@gmail.com>
2022-06-13 16:23:58 -0500

    add qr code

--- hs_pos_fe/models/electronic_invoice_pos.py
+++ hs_pos_fe/models/electronic_invoice_pos.py
@@ -1,13 +1,30 @@
 # -*- coding: utf-8 -*-
-
+import base64
 from odoo import models, fields, api
-
+import qrcode
 import logging
+from io import BytesIO
+
 _logger = logging.getLogger(__name__)
 
 
 class PosOrder(models.Model):
     _inherit = "pos.order"
+
+    def generate_qr(self, strQR):
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(strQR)
+        qr.make(fit=True)
+        img = qr.make_image()
+        temp = BytesIO()
+        img.save(temp, format="PNG")
+        qr_image = base64.b64encode(temp.getvalue())
+        self.qr_code = qr_image
 
     def action_pos_order_invoice(self):
         act_window = super(PosOrder, self).action_pos_order_invoice()
@@ -28,23 +45,8 @@ class PosOrder(models.Model):
                         if str(fe_info[info]) == 'True':
                             order.account_move.send_fiscal_doc()
                             order.include_pos = str(fe_info[info])
-                            order.qr_code = str(qr)
+                            #order.qr_code = str(qr)
+                            self.generate_qr(str(qr))
                             order.CAFE = str(cafe)
-
-                """ for info in fe_info:
-
-                    if info == 'cafe':
-                        cafe = fe_info[info]
-
-                    if info == 'qr':
-                        qr = fe_info[info]
-
-                    if info == 'isPOS':
-                        if fe_info[info]:
-                            order.account_move.send_fiscal_doc()
-                            order.include_pos = fe_info[info]
-                            order.qr_code = qr
-                            order.CAFE = cafe
-                            logging.info("El CAFE del FE:" + order.CAFE) """
 
         return act_window
