5954dbafa94c68f9c6b4158ef9cb378b146ce32b
enrike14 <evelasquez1485@gmail.com>
2022-06-14 15:31:23 -0500

    add await

--- hs_pos_fe/static/src/js/pos.js
+++ hs_pos_fe/static/src/js/pos.js
@@ -92,7 +92,7 @@ odoo.define("pos_fe.screens", function (require) {
       var self = this;
       var order = this.pos.get_order();
       var receipt_data = order.export_for_printing();
-      var qrfe = self.getfevalues();
+      var qrfe = await self.getfevalues();
       receipt_data.qr = qrfe;
       console.log("DATA::::::::" + receipt_data);
 
