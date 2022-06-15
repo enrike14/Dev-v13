e836fd77fa13e6bc3249c1221d19c1dcb121fccc
enrike14 <evelasquez1485@gmail.com>
2022-06-15 09:03:46 -0500

    add extend

--- hs_pos_fe/static/src/js/receipt.js
+++ hs_pos_fe/static/src/js/receipt.js
@@ -3,7 +3,7 @@ odoo.define("pos_fe.receipt", function (require) {
   var models = require("point_of_sale.models");
   models.load_fields("pos.order", ["cufe", "qr_code", "include_pos"]);
   var _super_orderline = models.Orderline.prototype;
-  models.Orderline.include({
+  models.Orderline = models.Orderline.extend({
     export_for_printing: function () {
       var line = _super_orderline.export_for_printing.apply(this, arguments);
       line.cufe = this.get_order().cufe;
