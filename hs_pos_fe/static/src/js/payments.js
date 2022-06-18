4fb93d2a8951720acc91905f24c136a71dc3ba37
enrike14 <evelasquez1485@gmail.com>
2022-06-18 10:03:17 -0500

    fix super

--- hs_pos_fe/static/src/js/payments.js
+++ hs_pos_fe/static/src/js/payments.js
@@ -1,24 +1,21 @@
 this.$(".js_invoice").removeClass("highlight");
 odoo.define("pos_fe.screens", function (require) {
   "use strict";
-
+  var session = require("web.session");
   var screens = require("point_of_sale.screens");
+  var rpc = require("web.rpc");
 
   screens.PaymentScreenWidget.include({
     click_invoice: function () {
-      this._super();
-      var self = this;
       var order = this.pos.get_order();
 
-      order.set_to_invoice(true);
+      order.set_to_invoice(!order.is_to_invoice());
 
       if (order.is_to_invoice()) {
         this.$(".js_invoice").addClass("highlight");
       } else {
         this.$(".js_invoice").removeClass("highlight");
       }
-
-      this._super();
     },
   });
 });
