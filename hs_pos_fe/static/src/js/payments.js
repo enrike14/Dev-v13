f2a1d246297d0a2f732fdc60bf9be8c5699ad90f
enrike14 <evelasquez1485@gmail.com>
2022-06-18 10:11:39 -0500

    fix super

--- hs_pos_fe/static/src/js/payments.js
+++ hs_pos_fe/static/src/js/payments.js
@@ -7,9 +7,11 @@ odoo.define("pos_fe.screens", function (require) {
 
   screens.PaymentScreenWidget.include({
     click_invoice: function () {
+      this._super();
       var order = this.pos.get_order();
 
-      order.set_to_invoice(!order.is_to_invoice());
+      //order.set_to_invoice(!order.is_to_invoice());
+      order.set_to_invoice(true);
 
       if (order.is_to_invoice()) {
         this.$(".js_invoice").addClass("highlight");
